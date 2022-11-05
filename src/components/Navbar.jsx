2594a2dbaff15ab8baec0d82303782fea55a4c7b
Javier Alejandro Rincón <110916364+RainRevan@users.noreply.github.com>
2022-11-05 16:22:07 -0500

    Actualización
    Se adiciona el trabajo sobre las pagina

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+function Navbar(props) {
+    return (
+        <div className="text-dark">
+            <header className="p-3 text-bg-dark bg-primary">
+                <div className="container">
+                    <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                            <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+                        </a>
+                        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                            <li><a href="#" class="nav-link px-2 text-dark">Nosotros</a></li>
+                            <li><a href="#" class="nav-link px-2 text-dark">Adopta</a></li>
+                            <li><a href="#" class="nav-link px-2 text-dark">Dudas</a></li>
+                            <li><a href="#" class="nav-link px-2 text-dark">Tienda</a></li>
+                            <li><a href="#" class="nav-link px-2 text-dark">Dona</a></li>
+                        </ul>
+                        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                            <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Buscar..." aria-label="Search" />
+                        </form>
+
+                        <div className="text-end">
+                            <button type="button" className="btn btn-outline-light me-2">Login</button>
+                            <button type="button" className="btn btn-warning">Registrarme</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
+export default Navbar;
---
+++ src/components/Nosotros.jsx
@@ -0,0 +1,12 @@
+function Nosotros(props) {
+    return (
+        <div>
+            <h1>Quienes Somos</h1>
+            <h2>Misión</h2>
+            <p>Misión El hogar de paso DivAngel, es una entidad sin ánimo de lucro, comprometida con promover mediante diferentes actividades la adopción responsable, la conciencia de esterilizar y recuperar animales en condición de maltrato y abandono con el fin de enseñar, contribuir y entregar una calidad de vida a todos los perros y gatos que lo necesiten.</p>
+                <h2>Visión</h2>
+                <p>Queremos ser reconocidos a nivel mundial por el impacto generado no solo a todos aquellos animales que fueron rescatados, sino a toda la sociedad por generar conocimiento y consciencia acerca de la importancia de esterilizar, y de la responsabilidad de tener un perro y/o gato en nuestro hogar.</p>
+        </div>
+    );
+}
+export default Nosotros;
